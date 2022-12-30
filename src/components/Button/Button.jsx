fabe3fa298b642db9b7842c43e37a3e37df41f2d
gash94 <104354207+gash94@users.noreply.github.com>
2022-12-30 14:24:13 +0100

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -5,7 +5,7 @@ import css from "./Button.module.css";
 export default function Button({ onClick }) {
     return (
         <button type="button" onClick={onClick} className={css.Button}>
-            Load morea
+            Load more
         </button>
     );
 }
